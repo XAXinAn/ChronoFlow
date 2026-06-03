1a340320e7e24ff4463599e6e2fb1aebac27395e
XAXinAn <xaxinan@github.com>
2026-06-03 19:16:10 +0800

    fix: 发布到选择器改用 getMyGroupTree(仅自己有权限的群组)

--- frontend/lib/pages/schedule/select_publish_target_page.dart
+++ frontend/lib/pages/schedule/select_publish_target_page.dart
@@ -24,7 +24,7 @@ class _SelectPublishTargetPageState extends State<SelectPublishTargetPage> {
 
   Future<void> _load() async {
     try {
-      final tree = await GroupService().getPublishTargetTree();
+      final tree = await GroupService().getMyGroupTree();
       setState(() { _myGroups = tree; _isLoading = false; });
     } catch (e) {
       setState(() => _isLoading = false);
