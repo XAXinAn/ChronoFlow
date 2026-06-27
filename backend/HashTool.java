e466d21d2767d3c5c1ee066b11e38702b4361d0b
XAXinAn <xaxinan@github.com>
2026-06-27 10:07:54 +0800

    chore: 删除临时文件

--- backend/HashTool.java
+++
@@ -1,3 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-public class HashTool { public static void main(String[] a) {
-System.out.println(new BCryptPasswordEncoder().encode("admin123")); } }
